1230a9f58276ad26585dddfde3e9310c3dead3b1
soyoung97 <lovelife@kaist.ac.kr>
2020-07-05 20:48:59 +0000

    Remove unnecessary files

--- src/training/simple_lstm.sh
+++
@@ -1,8 +0,0 @@
-fairseq-train \
-    ./../../data/binarized/korean_learner \
-    --user-dir ./../fairseq/models \
-    --arch simple_lstm_seq2seq \
-    --lr 1e-4 --clip-norm 0.1 --max-tokens 4000 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-    --lr-scheduler fixed \
-    --save-dir ./../../checkpoint/LSTM/korean_learner
