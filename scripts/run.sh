db5532a0c49c0573fe103c18262fca22ecda6d58
soyoung97 <lovelife@kaist.ac.kr>
2020-07-07 00:06:09 +0000

    Fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
 mkdir models
 for DATASET in dev-clean  dev-other  test-clean  test-other
 do
-		python score.py --ORIG_PATH examples/asr-librispeech-espnet/data/{$DATASET}.am.json --PPPL_SCORE
-		python score.py --ORIG_PATH examples/asr-librispeech-espnet/data/{$DATASET}.am.json \
-				--PPPL_PATH examples/asr-librispeech-espnet/data/{$DATASET}.am.pppl.json --RESCORE
+		python score.py --ORIG_PATH examples/asr-librispeech-espnet/data/$DATASET.am.json --PPPL_SCORE
+		python score.py --ORIG_PATH examples/asr-librispeech-espnet/data/$DATASET.am.json \
+				--PPPL_PATH examples/asr-librispeech-espnet/data/$DATASET.am.pppl.json --RESCORE
+
+done
